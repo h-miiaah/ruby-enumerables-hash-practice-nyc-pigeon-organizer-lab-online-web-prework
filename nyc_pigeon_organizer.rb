d1048dd4eaa2bd1027856c2828b468bb62efa1af
Haim D <haim11293@aol.com>
2019-07-24 00:59:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -42,7 +42,26 @@ def nyc_pigeon_organizer(data)
     end
   end
 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_pigeon_data[item][:gender] << gender.to_s
+        end
+      end
+    end
+  end
 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_pigeon_data[item][:lives] << location
+        end
+      end
+    end
+  end
 
+  return new_pigeon_data
 
 end
